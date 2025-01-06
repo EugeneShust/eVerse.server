9fca343142f14bc6a8af7a5a9cab03452af71405
Yevhen <eugene.shust@gmail.com>
2025-01-06 19:49:26 +0100

    Added Core.Firebase

---
+++ api.server/Core/Firebase/StartupExtentions.cs
@@ -0,0 +1,30 @@
+﻿using Core.Mongo;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+
+namespace Core.Firebase
+{
+    public static class StartupExtentions
+    {
+        public static IServiceCollection AddFirebaseApp(this IServiceCollection services, IConfiguration configuration)
+        {
+            var path = configuration.GetValue<string>("FirebaseConfigPath");
+
+            services.AddSingleton<FirebaseApp>(sp =>
+            {
+                if (FirebaseApp.DefaultInstance == null)
+                {
+                    var path = sp.GetRequiredService<IConfiguration>().GetValue<string>("FirebaseConfigPath");
+                    return FirebaseApp.Create(new AppOptions
+                    {
+                        Credential = GoogleCredential.FromFile(path)
+                    });
+                }
+
+                return FirebaseApp.DefaultInstance;
+            });
+
+            return services;
+        }
+    }
+}
