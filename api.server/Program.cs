4b875ebd9323b79436fafd234cd9a29807d1fb17
Yevhen <eugene.shust@gmail.com>
2025-01-06 19:52:02 +0100

    Added CORS policy

--- api.server/Program.cs
+++ api.server/Program.cs
@@ -8,6 +8,16 @@ namespace api.server
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigins", policy =>
+                {
+                    policy.WithOrigins("http://localhost:5173")
+                          .AllowAnyHeader() 
+                          .AllowAnyMethod();
+                });
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -15,6 +25,8 @@ namespace api.server
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+            
+            app.UseCors("AllowSpecificOrigins");
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
