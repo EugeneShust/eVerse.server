8eaf09a3106a812bc763e8d448471cb9e133297b
Yevhen <eugene.shust@gmail.com>
2025-01-11 19:19:35 +0100

    added HttpContextExtensions

---
+++ api.server/Internal/Extentions/HttpContextExtensions.cs
@@ -0,0 +1,23 @@
+﻿using FirebaseAdmin.Auth;
+
+namespace Internal.Extentions
+{
+    // todo partial class
+    public static class HttpContextExtensions
+    {
+        public static FirebaseToken? GetFirebaseToken(this HttpContext context)
+        {
+            return context.Items["FirebaseToken"] as FirebaseToken;
+        }
+
+        public static string? GetUserId(this HttpContext context)
+        {
+            if (context.User == null || !context.User.Identity.IsAuthenticated)
+            {
+                return null;
+            }
+
+            return context.User.FindFirst("id")?.Value;
+        }
+    }
+}
